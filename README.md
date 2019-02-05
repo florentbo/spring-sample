881569843cc8cdef49fcfaf473cd6ae44ad66315
florent <florent@bonamis.be>
2019-02-05 18:31:46 +0100

    initialisation du projet

---
+++ README.md
@@ -0,0 +1,4 @@
+Spring boot jersey sample
+===
+
+Projet permettant de démarrer un projet spring boot rest en utilisant JERSEY comme implémentation de JAX-RS
---
+++ src/main/java/com/bonamis/springjersey/SpringJerseyApplication.java
@@ -0,0 +1,14 @@
+package com.bonamis.springjersey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJerseyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJerseyApplication.class, args);
+    }
+
+}
+
