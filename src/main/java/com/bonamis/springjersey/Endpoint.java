5a5b66f9e560ff0a88aef15b1571f8b202a3a2e5
florent <florent@bonamis.be>
2019-02-05 21:12:29 +0100

    ajout de la configuration jersey nécessaire

---
+++ src/main/java/com/bonamis/springjersey/Endpoint.java
@@ -0,0 +1,18 @@
+package com.bonamis.springjersey;
+
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Component
+@Path("/hello")
+public class Endpoint {
+
+
+    @GET
+    public String message() {
+        return "Hello " + "message";
+    }
+
+}
--- src/main/java/com/bonamis/springjersey/SpringJerseyApplication.java
+++ src/main/java/com/bonamis/springjersey/SpringJerseyApplication.java
@@ -1,14 +1,19 @@
 package com.bonamis.springjersey;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringJerseyApplication {
+public class SpringJerseyApplication  extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringJerseyApplication.class, args);
-    }
+        public static void main(String[] args) {
+            new SpringJerseyApplication()
+                    .configure(new SpringApplicationBuilder(SpringJerseyApplication.class))
+                    .run(args);
+        }
+
+
 
 }
 
